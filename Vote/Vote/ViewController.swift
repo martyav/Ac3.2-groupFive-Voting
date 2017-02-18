b5668daccdafbe2edfc8493c8f16b660ad685511
Liam <liamdkane@gmail.com>
2017-02-18 09:25:53 -0500

    first commit

---
+++ Vote/Vote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vote
+//
+//  Created by C4Q on 2/18/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
