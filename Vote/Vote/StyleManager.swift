23b43b339f71768ad2460462c2c01bc189fb4217
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-02-18 18:24:33 -0500

    style manager is up and running...check out the extension on uicolors

--- Vote/Vote/StyleManager.swift
+++ Vote/Vote/StyleManager.swift
@@ -8,11 +8,73 @@
 
 import UIKit
 
+extension UIColor {
+    
+}
+
 class StyleManager {
     static let styler = StyleManager()
     
     private init() {}
     
-    
+    func prettify() {
+        let boldFont = "GillSans-Bold"
+        let semiboldFont = "GillSans-SemiBold"
+        let regularFont = "GillSans"
+        let italicFont = "GillSans-Italic"
+        let lightFont = "GillSans-LightItalic"
+        
+        // top bar
+        let proxyNavBar = UINavigationBar.appearance()
+        let proxyBarButton = UIBarButtonItem.appearance()
+        // details & text
+        let proxyImageView = UIImageView.appearance()
+        let proxyLabel = UILabel.appearance()
+        let proxyTextField = UITextField.appearance()
+        let proxyTextView = UITextView.appearance()
+        let proxyPlaceholder = UILabel.appearance(whenContainedInInstancesOf: [UITextField.self])
+        // generalized info
+        let proxyView = UIView.appearance()
+        let proxyWebView = UIWebView.appearance()
+        let proxyTableView = UITableView.appearance()
+        let proxyCell = UITableViewCell.appearance()
+        let proxyScrollView = UIScrollView.appearance()
+        
+        // top bar styling
+        proxyNavBar.backgroundColor = UIColor.hackathonBlue
+        proxyNavBar.tintColor = UIColor.hackathonCream
+        
+        // detail & text styling
+        proxyLabel.textColor = UIColor.hackathonBlack
+        proxyLabel.font = UIFont(name: semiboldFont, size: 20)
+        proxyLabel.textColor = UIColor.hackathonGrey
+        
+        proxyTextView.font = UIFont(name: regularFont, size: 16)
+        proxyTextView.textColor = UIColor.hackathonBlack
+        
+        proxyTextField.backgroundColor = UIColor.hackathonCream
+        proxyTextField.textColor = UIColor.hackathonGrey
+        proxyTextField.font = UIFont(name: semiboldFont, size: 20)
+        proxyTextField.layer.cornerRadius = 3.0
+        proxyPlaceholder.font = UIFont(name: lightFont, size: 20)
+        proxyPlaceholder.backgroundColor = proxyTextField.backgroundColor
+        proxyTextField.text = "90210"
+        
+        // generalized info styling
+        proxyView.backgroundColor = UIColor.hackathonWhite
+        proxyWebView.scalesPageToFit = true
+        proxyWebView.scrollView.bounces = true
+        proxyWebView.layer.cornerRadius = 3.0
+        proxyWebView.layer.borderColor = UIColor.hackathonGrey.cgColor
+        proxyWebView.layer.borderWidth = 1.0
+        
+        proxyTableView.backgroundColor = UIColor.hackathonCream
+        proxyTableView.tableHeaderView?.backgroundColor = UIColor.hackathonRed
+        proxyTableView.tableHeaderView?.tintColor = UIColor.hackathonCream
+        
+        proxyScrollView.bounces = true
+        proxyScrollView.backgroundColor = UIColor.hackathonCream
+        proxyScrollView.tintColor = UIColor.hackathonRed
+    }
     
 }
