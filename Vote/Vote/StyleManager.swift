9ed4ec38827ca480d375e602782a0fc5b1151add
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-02-18 16:31:31 -0500

    added style manager and placeholder

---
+++ Vote/Vote/StyleManager.swift
@@ -0,0 +1,18 @@
+//
+//  StyleManager.swift
+//  Vote
+//
+//  Created by Marty Avedon on 2/18/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class StyleManager {
+    static let styler = StyleManager()
+    
+    private init() {}
+    
+    
+    
+}
