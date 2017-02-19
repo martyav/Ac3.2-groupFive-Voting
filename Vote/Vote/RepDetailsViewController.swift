5053357b40811cc9fc4ffe9a6faa3e8649d35008
SimoneGrant <simonegrant001@gmail.com>
2017-02-19 13:54:56 -0500

    Web view

--- Vote/Vote/RepDetailsViewController.swift
+++ Vote/Vote/RepDetailsViewController.swift
@@ -27,7 +27,7 @@ class RepDetailsViewController: UIViewController, UICollectionViewDelegate, UICo
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       print(official.photoURL)
+        print(official.photoURL)
         
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
         inputViewValues()
@@ -53,22 +53,13 @@ class RepDetailsViewController: UIViewController, UICollectionViewDelegate, UICo
     override func viewDidLayoutSubviews() {
         self.repImageView.layer.cornerRadius = 60
         self.repImageView.clipsToBounds = true
-
+        
     }
     
     func inputViewValues () {
         self.repNameLabel.text = official.name
         self.phoneNumberLabel.text = official.phone
         self.emailLabel.text = official.email
-<<<<<<< HEAD
-
-        
-        APIRequestManager.manager.getImage(APIEndpoint: official.photoURL!) { (data) in
-            if let validData = data,
-                let validImage = UIImage(data: validData) {
-                DispatchQueue.main.async {
-                    self.repImageView.image = validImage
-=======
         self.repImageView.image = UIImage(named: "placeholderPic")
         
         if let phoneNumber = official.phone {
@@ -84,51 +75,29 @@ class RepDetailsViewController: UIViewController, UICollectionViewDelegate, UICo
                         self.repImageView.image = validImage
                         
                     }
->>>>>>> 94cb75a9b03ce490770aa7f781ee7888efd98f4a
                 }
             }
         }
-        
-<<<<<<< HEAD
-       
-//        self.iconImageView = {
-//            let imageView = UIImageView()
-//            switch self.official.party {
-//            case _ where self.official.party.contains("Democrat"):
-//                self.iconImageView.image = #imageLiteral(resourceName: "democrat")
-//            case "Republican":
-//                self.iconImageView.image = #imageLiteral(resourceName: "republican")
-//            default:
-//                self.iconImageView.image = #imageLiteral(resourceName: "defaultParty")
-//            }
-//            imageView.contentMode = .center
-//            imageView.backgroundColor = UIColor.hackathonWhite
-//            imageView.layer.cornerRadius = 20
-//            imageView.layer.borderColor = UIColor.hackathonBlue.cgColor
-//            imageView.layer.borderWidth = 0.75
-//            return imageView
-//        }()
-=======
-        self.iconImageView = {
-            let imageView = UIImageView()
-            switch self.official.party {
-            case _ where self.official.party.contains("Democrat"):
-                self.iconImageView.image = #imageLiteral(resourceName: "democrat")
-            case "Republican":
-                self.iconImageView.image = #imageLiteral(resourceName: "republican")
-            default:
-                self.iconImageView.image = #imageLiteral(resourceName: "defaultParty")
-            }
-            imageView.contentMode = .center
-            imageView.backgroundColor = UIColor.hackathonWhite
-            imageView.layer.cornerRadius = 20
-            imageView.layer.borderColor = UIColor.hackathonBlue.cgColor
-            imageView.layer.borderWidth = 0.75
-            
-            return imageView
-        }()
->>>>>>> 94cb75a9b03ce490770aa7f781ee7888efd98f4a
     }
+    
+        
+        //        self.iconImageView = {
+        //            let imageView = UIImageView()
+        //            switch self.official.party {
+        //            case _ where self.official.party.contains("Democrat"):
+        //                self.iconImageView.image = #imageLiteral(resourceName: "democrat")
+        //            case "Republican":
+        //                self.iconImageView.image = #imageLiteral(resourceName: "republican")
+        //            default:
+        //                self.iconImageView.image = #imageLiteral(resourceName: "defaultParty")
+        //            }
+        //            imageView.contentMode = .center
+        //            imageView.backgroundColor = UIColor.hackathonWhite
+        //            imageView.layer.cornerRadius = 20
+        //            imageView.layer.borderColor = UIColor.hackathonBlue.cgColor
+        //            imageView.layer.borderWidth = 0.75
+        //            return imageView
+        //        }()
     
     /*
      // MARK: - Navigation
@@ -168,5 +137,6 @@ class RepDetailsViewController: UIViewController, UICollectionViewDelegate, UICo
             }
         }
     }
-    
 }
+
+
