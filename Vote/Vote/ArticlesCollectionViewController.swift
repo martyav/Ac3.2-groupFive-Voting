3555e44fe97e4fbd93e73c5049f8c1d5b49d9364
Liam <liamdkane@gmail.com>
2017-02-19 12:00:43 -0500

    ch-ch-ch-changeeees

--- Vote/Vote/ArticlesCollectionViewController.swift
+++ Vote/Vote/ArticlesCollectionViewController.swift
@@ -57,9 +57,6 @@ class ArticlesCollectionViewController: UICollectionViewController {
     
         return cell
     }
-
-
-
 }
 
 
--- Vote/Vote/RepresentativeTableViewController.swift
+++ Vote/Vote/RepresentativeTableViewController.swift
@@ -29,7 +29,7 @@ class RepresentativeTableViewController: UITableViewController {
             if let validInfo = info {
                 dump(validInfo.offices.count)
                 dump(validInfo.officials.count)
-                self.office = validInfo.offices
+                self.office = validInfo.offices.reversed()
                 self.repDetails = validInfo.officials
                 
                 DispatchQueue.main.async {
